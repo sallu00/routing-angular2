f4c315821590c3ba609858f0d4ec076e3fadd34b
Neeraj <neeraj.17011996@gmail.com>
2017-06-09 19:42:19 +0530

    observables part done

--- src/app/profile/contact/contact.component.ts
+++ src/app/profile/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Http , Response } from '@angular/http' ;
+import { Http, Response } from '@angular/http';
 import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-contact',
@@ -8,26 +8,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  
+
   contact1: FormGroup;
   fb: FormBuilder;
   userData: any;
   users: object;
-  constructor(fb: FormBuilder , public http: Http , private route: ActivatedRoute) {
+  constructor(fb: FormBuilder, public http: Http, private route: ActivatedRoute) {
     this.contact1 = fb.group({
-      contact: [ '', Validators.required ]
+      contact: ['', Validators.required]
     });
   }
-  ngOnInit():void {
-    
-  }
-  makeRequest(){
-    this.http.get('/assets/jsonData/users.json')
-    .subscribe((res : Response) => { 
-      this.userData=res.json();
-      console.log(this.userData[0].firstName);
-    })
-  }
-  
+  ngOnInit(): void {
 
+  }
+  makeRequest() {
+    this.http.get('/assets/jsonData/users.json')
+      .map((res: Response) => { 
+        return res = res.json(); //transform data from http response to javascript object
+      })
+      .subscribe((res) => {
+        this.userData = res;
+      })
+  }
 }
